fac6d3cd5f17e2bffbc20b35517858f15d7f3237
Keith Holliday <krh121791@gmail.com>
2017-02-03 10:10:44 -0700

    Added leaderboard menu and removed hardcoded menu

--- website/client-old/js/controllers/leaderboardCtrl.js
+++ website/client-old/js/controllers/leaderboardCtrl.js
@@ -5,30 +5,6 @@ angular.module('habitrpg')
 
       // An array of lederboards used for the menu. For now, this is a hardcoded list of Challenge ids
       $scope.leaderboards = [
-        {
-          id: 'de61fb54-d200-42bc-a917-70b70d8cd408',
-          title: 'Power Quality and Reliability',
-        },
-        {
-          id: '033c5270-0cc6-4a51-b62d-e61450d1fde8',
-          title: 'Price',
-        },
-        {
-          id: 'cb0c29f1-7468-4126-876e-f6b97881016a',
-          title: 'Billing and Pay',
-        },
-        {
-          id: 'e1661681-d25c-4471-a4cb-e6eb25915f66',
-          title: 'Corporate Citizenship',
-        },
-        {
-          id: '0afd0d36-39e4-49a0-ad2f-fa7479df99e9',
-          title: 'Communications',
-        },
-        {
-          id: 'd9f86ef5-46ea-4634-acee-ac4aff67b89d',
-          title: 'Customer Service',
-        },
       ];
 
       var apiV3Prefix = '/api/v3';
