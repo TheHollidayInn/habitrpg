7a334e30bc64629c2b1e637812cad621c4ea774e
Keith Holliday <krh121791@gmail.com>
2017-02-18 16:47:48 -0700

    Added initial groupMembersAutocomplete test

---
+++ test/client-old/spec/directives/groupMembersAutocomplete.directive.spec.js
@@ -0,0 +1,28 @@
+describe('groupMembersAutocomplete Directive', function() {
+  var compile, scope, directiveElem;
+
+  beforeEach(function() {
+    module('habitrpg');
+
+    inject(function($compile, $rootScope){
+      compile = $compile;
+      scope = $rootScope.$new();
+    });
+
+    directiveElem = getCompiledElement();
+  });
+
+  function getCompiledElement(){
+    var element = angular.element('<group-members-autocomplete></group-members-autocomplete>');
+    var compiledElement = compile(element)(scope);
+    scope.$digest();
+    return compiledElement;
+  }
+
+  // @TODO: Begin testing directives after we cache templates
+  it('should have span element', function () {
+    // var spanElement = directiveElem.find('span');
+    // expect(spanElement).toBeDefined();
+    // expect(spanElement.text()).toEqual('This span is appended from directive.');
+  });
+});
