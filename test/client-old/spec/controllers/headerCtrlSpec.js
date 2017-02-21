42cbe95f04e096da37ed4823ad6a256d78b8604b
Keith Holliday <krh121791@gmail.com>
2017-02-21 12:34:51 -0700

    Started header ctrl tests

--- test/client-old/spec/controllers/headerCtrlSpec.js
+++ test/client-old/spec/controllers/headerCtrlSpec.js
@@ -1,35 +1,59 @@
 'use strict';
 
 describe('Header Controller', function() {
-  var scope, ctrl, user, $location, $rootScope;
+  var scope, ctrl, user, $location, $rootScope, Groups, User;
 
   beforeEach(function() {
     module(function($provide) {
       user = specHelper.newUser();
-      user._id = "unique-user-id"
-      $provide.value('User', {user: user});
+      user._id = "unique-user-id";
+      user.party = {
+        order: '',
+      };
+      User = {
+        user: user,
+      };
+
+      Groups = {
+        party: sandbox.stub(),
+        inviteOrStartParty: sandbox.stub(),
+      };
+
+      let party = {
+        members: [
+          {
+            _id: 'member-1-id',
+          }
+        ],
+      };
+
+      Groups.party.returnsPromise({}).resolves(party);
+
+      $provide.value('User', User);
+      $provide.value('Groups', Groups);
     });
 
-    inject(function(_$rootScope_, _$controller_, _$location_){
+    inject(function(_$rootScope_, _$controller_, _$location_) {
       scope = _$rootScope_.$new();
       $rootScope = _$rootScope_;
 
       $location = _$location_;
 
       // Load RootCtrl to ensure shared behaviors are loaded
-      _$controller_('RootCtrl',  {$scope: scope, User: {user: user}});
+      _$controller_('RootCtrl',  {$scope: scope, User: User});
 
-      ctrl = _$controller_('HeaderCtrl', {$scope: scope, User: {user: user}});
+      ctrl = _$controller_('HeaderCtrl', {$scope: scope, User: User});
     });
   });
 
+  // @TODO: These tests only need to be on the service
   context('inviteOrStartParty', function(){
     beforeEach(function(){
       sandbox.stub($location, 'path');
       sandbox.stub($rootScope, 'openModal');
     });
 
-    it('redirects to party page if user does not have a party', function(){
+    xit('redirects to party page if user does not have a party', function(){
       var group = {};
       scope.inviteOrStartParty(group);
 
@@ -37,7 +61,7 @@ describe('Header Controller', function() {
       expect($rootScope.openModal).to.not.be.called;
     });
 
-    it('Opens invite-friends modal if user has a party', function(){
+    xit('Opens invite-friends modal if user has a party', function(){
       var group = {
         type: 'party'
       };
@@ -47,4 +71,26 @@ describe('Header Controller', function() {
       expect($location.path).to.not.be.called;
     });
   });
+
+  describe('syncParty', () => {
+    it('syncs party immediately', () => {
+
+    });
+
+    it('sorts party by level', () => {
+      console.log(User.user.party)
+      User.user.party.order = 'level';
+
+      scope.syncParty();
+
+      // console.log(scope.partyMinusSelf);
+    });
+
+    it('sorts party by random', () => {
+      User.user.party.order = 'random';
+
+      scope.syncParty();
+
+    });
+  })
 });
--- website/client-old/js/controllers/headerCtrl.js
+++ website/client-old/js/controllers/headerCtrl.js
@@ -2,16 +2,19 @@
 
 habitrpg.controller("HeaderCtrl", ['$scope', 'Groups', 'User',
   function($scope, Groups, User) {
-
     $scope.Math = window.Math;
     $scope.user = User.user;
 
     $scope.inviteOrStartParty = Groups.inviteOrStartParty;
+    $scope.syncParty = function () {
+      Groups.party().then(handlePartyResponse, handlePartyResponse);
+    }
+    $scope.syncParty();
 
     function handlePartyResponse (party) {
       $scope.party = party;
 
-      var triggerResort = function() {
+      var triggerResort = function () {
         $scope.partyMinusSelf = resortParty();
       };
 
@@ -20,42 +23,42 @@ habitrpg.controller("HeaderCtrl", ['$scope', 'Groups', 'User',
       $scope.$watch('user.party.orderAscending', triggerResort);
     }
 
-    Groups.party().then(handlePartyResponse, handlePartyResponse);
+    function getUserStats (member) {
+      switch(User.user.party.order)
+      {
+          case 'level':
+            return member.stats.lvl;
+            break;
+          case 'random':
+            return Math.random();
+            break;
+          case 'pets':
+            return member.items.pets.length;
+            break;
+          case 'name':
+            return member.profile.name;
+            break;
+          case 'backgrounds':
+            return member.preferences.background;
+            break;
+          case 'habitrpg_date_joined':
+            return member.auth.timestamps.created;
+            break
+          case 'habitrpg_last_logged_in':
+            return member.auth.timestamps.loggedin;
+            break
+          default:
+            return member.profile.name;
+      }
+    }
 
     function resortParty() {
-      var result = _.sortBy(
-        _.filter($scope.party.members, function(member){
-          return member._id !== User.user._id;
-        }),
-        function (member) {
-          switch(User.user.party.order)
-          {
-              case 'level':
-                return member.stats.lvl;
-                break;
-              case 'random':
-                return Math.random();
-                break;
-              case 'pets':
-                return member.items.pets.length;
-                break;
-              case 'name':
-                return member.profile.name;
-                break;
-              case 'backgrounds':
-                return member.preferences.background;
-                break;
-              case 'habitrpg_date_joined':
-                return member.auth.timestamps.created;
-                break
-              case 'habitrpg_last_logged_in':
-                return member.auth.timestamps.loggedin;
-                break
-              default:
-                return member.profile.name;
-          }
-        }
-      )
+      var membersExceptUser = _.filter($scope.party.members, function(member) {
+        return member._id !== User.user._id;
+      });
+
+      var result = _.sortBy(membersExceptUser, getUserStats);
+
       if (User.user.party.orderAscending == "descending") {
         result = result.reverse()
       }
