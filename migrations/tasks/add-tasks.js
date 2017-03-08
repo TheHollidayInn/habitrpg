292b0f87d3219c29e597ab46ca7234faac86787e
Keith Holliday <krh121791@gmail.com>
2017-03-08 12:31:58 -0700

    Migration updated

--- migrations/tasks/add-tasks.js
+++ migrations/tasks/add-tasks.js
@@ -5,37 +5,45 @@ import * as Tasks from '../../website/server/models/task';
 import { model as Challenges } from '../../website/server/models/challenge';
 
 let tasks = [
+  // {
+  //   text: '**Take a picture of yourself** next to an appliance with a sign showing how much it costs to run it in a normal month. Post it to your favorite social media channel (Facebook, Twitter, Instagram or Pinterest) and use tags *@ComEd, #DollarPower, #Emp*.',
+  //   type: 'habit',
+  //   up: true,
+  //   down: false,
+  //   challengeId: '033c5270-0cc6-4a51-b62d-e61450d1fde8',
+  //   challenge: {
+  //     id: '033c5270-0cc6-4a51-b62d-e61450d1fde8',
+  //     shortName: 'Price',
+  //   },
+  //   question: 'Total monthly cost of your electric service',
+  //   value: 40,
+  // },
   {
-    text: '**Take a picture of yourself** next to an appliance with a sign showing how much it costs to run it in a normal month. Post it to your favorite social media channel (Facebook, Twitter, Instagram or Pinterest) and use tags *@ComEd, #DollarPower, #Emp*.',
+    text: '**Participate in a ComEd charity or organization** event and log your volunteer hours.   Download the Energy for the Community Mobile App. *Use tags  #ComEdVolunteers, #WeAreComEd*',
     type: 'habit',
     up: true,
     down: false,
-    challengeId: '92c2e13a-3f71-40b0-ac81-c8087568686c', //033c5270-0cc6-4a51-b62d-e61450d1fde8',
+    challengeId: 'e1661681-d25c-4471-a4cb-e6eb25915f66',
     challenge: {
-      id: '92c2e13a-3f71-40b0-ac81-c8087568686c', //033c5270-0cc6-4a51-b62d-e61450d1fde8',
+      // id: '033c5270-0cc6-4a51-b62d-e61450d1fde8',
       shortName: 'Price',
     },
-    question: 'Total monthly cost of your electric service',
-    value: 40,
+    question: 'Corporate Citizenship',
+    value: 50,
   },
-  // {
-  //   text: '**Participate in a ComEd charity or organization** event and log your volunteer hours.   Download the Energy for the Community Mobile App. *Use tags  #ComEdVolunteers, #WeAreComEd*',
-  //   type: 'habit',
-  //   up: true,
-  //   down: false,
-  //   challengeId: 'e1661681-d25c-4471-a4cb-e6eb25915f66',
-  //   question: 'Involvement in local charities and civic organizations',
-  //   value: 50,
-  // },
-  // {
-  //   text: '**How do You Power Lives?** We are all in  Customer Service even if you don\'t deal directly with the customers. What you do powers lives. Review the [Leadership and Values tab](https://www.comed.com/AboutUs/Pages/LeadershipValues.aspx) at ComEd.com then tell us how you POWER LIVES in the pop up.',
-  //   type: 'habit',
-  //   up: true,
-  //   down: false,
-  //   challengeId: 'd9f86ef5-46ea-4634-acee-ac4aff67b89d',
-  //   question: 'Representative\'s knowledge',
-  //   value: 20,
-  // },
+  {
+    text: '**How do You Power Lives?** We are all in  Customer Service even if you don\'t deal directly with the customers. What you do powers lives. Review the [Leadership and Values tab](https://www.comed.com/AboutUs/Pages/LeadershipValues.aspx) at ComEd.com then tell us how you POWER LIVES in the pop up.',
+    type: 'habit',
+    up: true,
+    down: false,
+    challengeId: 'd9f86ef5-46ea-4634-acee-ac4aff67b89d',
+    challenge: {
+      // id: '033c5270-0cc6-4a51-b62d-e61450d1fde8',
+      shortName: 'Price',
+    },
+    question: 'Customer Service',
+    value: 20,
+  },
 ];
 
 module.exports = async function uploadTasks () {
