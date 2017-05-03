6b7cd69b88f67c56a8925c82b7bde9c1e8b8fa80
Keith Holliday <krh121791@gmail.com>
2017-05-02 21:43:53 -0600

    Updated amplitude

--- website/client-old/js/services/analyticsServices.js
+++ website/client-old/js/services/analyticsServices.js
@@ -16,14 +16,35 @@
 
     var user = User.user;
 
-    // Amplitude
-    var r = window.amplitude || {};
-    r._q = [];
-    function a(window) {r[window] = function() {r._q.push([window].concat(Array.prototype.slice.call(arguments, 0)));}}
-    var i = ["init", "logEvent", "logRevenue", "setUserId", "setUserProperties", "setOptOut", "setVersionName", "setDomain", "setDeviceId", "setGlobalUserProperties"];
-    for (var o = 0; o < i.length; o++) {a(i[o])}
-    window.amplitude = r;
-    amplitude.init(window.env.AMPLITUDE_KEY, user ? user._id : undefined);
+    //Amplitude
+    // var r = window.amplitude || {};
+    // r._q = [];
+    // function a(window) {r[window] = function() {r._q.push([window].concat(Array.prototype.slice.call(arguments, 0)));}}
+    // var i = ["init", "logEvent", "logRevenue", "setUserId", "setUserProperties", "setOptOut", "setVersionName", "setDomain", "setDeviceId", "setGlobalUserProperties"];
+    // for (var o = 0; o < i.length; o++) {a(i[o])}
+    // window.amplitude = r;
+    // amplitude.init(window.env.AMPLITUDE_KEY, user ? user._id : undefined, null, null, function (res, err) {
+    //   console.log(res, err)
+    // });
+    //
+    // console.log(window.amplitude);
+
+
+    (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script");r.type="text/javascript";
+    r.async=true;r.src="https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-3.4.0-min.gz.js";
+    r.onload=function(){e.amplitude.runQueuedFunctions()};var i=t.getElementsByTagName("script")[0];
+    i.parentNode.insertBefore(r,i);function s(e,t){e.prototype[t]=function(){this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
+    return this}}var o=function(){this._q=[];return this};var a=["add","append","clearAll","prepend","set","setOnce","unset"];
+    for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[];return this;
+    };var p=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"];
+    for(var l=0;l<p.length;l++){s(c,p[l])}n.Revenue=c;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp","logEventWithGroups"];
+    function v(e){function t(t){e[t]=function(){e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
+    }}for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){e=(!e||e.length===0?"$default_instance":e).toLowerCase();
+    if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]};e.amplitude=n;
+    })(window,document);
+
+    amplitude.getInstance().init(window.env.AMPLITUDE_KEY);
+
 
     // Google Analytics (aka Universal Analytics)
     window['GoogleAnalyticsObject'] = 'ga';
@@ -70,7 +91,15 @@
         if(_doesNotHaveRequiredFields(properties)) { return false; }
         if(_doesNotHaveAllowedHitType(properties)) { return false; }
 
-        amplitude.logEvent(properties.eventAction,properties);
+        var result = amplitude.logEvent(properties.eventAction, properties, function (result, err) {
+          console.log(result, err)
+        })
+          // .then((result) {
+          //   console.log(result)
+          // })
+          // .catch((error) => {
+          //   console.log(error)
+          // })
         ga('send',properties);
       });
     }
